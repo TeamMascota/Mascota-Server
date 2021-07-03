ec45f535c59670ac59b61a7657f89ec8912e0b4f
hyunjong_96 <ahajongs@gmail.com>
2021-07-03 14:17:45 +0900

    feat : populate Issue 해결

--- src/interfaces/user/IUser.ts
+++ src/interfaces/user/IUser.ts
@@ -1,22 +1,22 @@
-import mongoose, {Document, Model, PopulatedDoc}from "mongoose"
+import mongoose, { Document, Model, PopulatedDoc } from "mongoose"
 import { IBook } from "../book/IBook"
-import {IPet} from "../pet/IPet"
+import { IPet } from "../pet/IPet"
 
-export interface IUser extends Document{
-    _id : mongoose.Types.ObjectId;
-    email : String;
-    password : String;
+export interface IUser extends Document {
+    _id: mongoose.Types.ObjectId;
+    email: String;
+    password: String;
     // pets?: PopulatedDoc<IPet & Document>;
     // book?: PopulatedDoc<IBook & Document>;
-   pets : [IPet];
-   book : [IBook];
+    pets: [IPet];
+    book: [IBook];
 }
 
-export interface IUserDocument extends IUser{
-    setPet : (pet : IPet) => Promise<void>
-    setBook : (book : IBook) => Promise<void>
+export interface IUserDocument extends IUser {
+    setPet: (pet: IPet) => Promise<void>
+    setBook: (book: IBook) => Promise<void>
 }
 
-export interface IUserModel extends Model<IUserDocument>{
+export interface IUserModel extends Model<IUserDocument> {
 
 }
