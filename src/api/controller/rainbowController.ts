107c6f6b33b8e92256c459dd70614bc19ed375a8
hyunjong_96 <ahajongs@gmail.com>
2021-07-04 02:23:58 +0900

    feat : 무지개 메인 홈 뷰

--- src/api/controller/rainbowController.ts
+++ src/api/controller/rainbowController.ts
@@ -3,7 +3,13 @@ const responseMessage = require('../../modules/responseMessage')
 const statusCode = require('../../modules/statusCode')
 
 module.exports = {
-    getRainbowMainView: async (req, res) => {
-        
+    mainPage: async (req, res) => {
+        const { userId, petId } = req.params;
+        try {
+             
+        } catch (err) {
+            console.error(err)
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR))
+        }
     }
 }
--- src/api/router/rainbow/index.ts
+++ src/api/router/rainbow/index.ts
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const rainbowController = require('../../controller/rainbowController')
 
-router.get('/main/:userId/:petId',rainbowController.getRainbowMainView) //1.5부 무지개 홈 화면
+router.get('/main/:userId/:petId',rainbowController.mainPage) //1.5부 무지개 홈 화면
 
 module.exports = router;
