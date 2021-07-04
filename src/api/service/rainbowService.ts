4816ccfa651cf007b37dccafc75a9269d8ebdb00
hyunjong_96 <ahajongs@gmail.com>
2021-07-05 01:21:48 +0900

    feat : trash

--- src/api/service/rainbowService.ts
+++ src/api/service/rainbowService.ts
@@ -13,38 +13,6 @@ require("../../models/etc/Help")
 
 module.exports = {
     getMainPage: async (userId, petId) => {
-        try {
-            const findUser = await User.findById(userId).populate({
-                path: "pets"
-            }).populate({
-                path: "book",
-                populate: {
-                    path: "tableContents",
-                    populate: { path: "firstPartTableContents" }
-                }
-            })
-            const findPet = await Pet.findById(petId);
-            console.log(findUser)
-            const firstPartTableContents = findUser.book.tableContents.firstPartTableContents
-            console.log('tableContents : ' + firstPartTableContents)
-            console.log('length : ' + firstPartTableContents.length)
-
-            console.log('test :'+ firstPartTableContents[0].petDiary.length)
-            //const validDiary = firstPartTableContents.filter(tableContents => tableContents.petDiary)
-            let memoriesResDto = [null,null]
-            // if(firstPartTableContents.length == 1){
-            //     memoriesResDto[0] = new MemoriesResDto(firstPartTableContents,findPet)
-            // }else{
-            //     memoriesResDto = 
-            // }
-            //helpResDto
-            const helps = await Help.find()
-            const helResDto = helps.map(help => new HelpResDto(help))
-
-            const result = findUser
-            return result
-        } catch (error) {
-            throw error
-        }
+        
     },
 }
