24cc24020cbf6cee155d92e724e8694a7be4fd58
sol lee <76844556+soleu@users.noreply.github.com>
2021-07-07 14:07:04 +0900

    petDiary 작성

--- src/api/router/index.ts
+++ src/api/router/index.ts
@@ -2,5 +2,5 @@ import express from "express"
 const router = express.Router()
 
 router.use('/book',require('./book')) //1부,2부 책
-
+router.use('/diary',require('./diary'));//1부 일기
 module.exports = router;
