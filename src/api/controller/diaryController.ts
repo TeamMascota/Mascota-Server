c53ffd86efbd9585c8b9532438bc6795ab9cadd1
sol lee <76844556+soleu@users.noreply.github.com>
2021-07-08 09:25:56 +0900

    feat: 1부 일기 수정, 삭제 API

--- src/api/controller/diaryController.ts
+++ src/api/controller/diaryController.ts
@@ -34,7 +34,36 @@ module.exports = {
     getPetDiary: async(req,res)=>{
         const petDiaryId=req.params;
         try {
-            const result = await diaryService.postPetDiary(petDiaryId)
+            const result = await diaryService.getPetDiary(petDiaryId)
+            res.status(statusCode.OK).send(util.success(statusCode.OK, result, ""))
+        } catch (err) {
+            console.error(err)
+            if (err.statusCode == null) {
+                err.statusCode = statusCode.INTERNAL_SERVER_ERROR;
+                err.responseMessage = responseMessage.INTERNAL_SERVER_ERROR;
+            }
+            return res.status(err.statusCode).send(util.fail(err.statusCode, err.responseMessage))
+        }
+    },
+    putPetDiary: async(req,res)=>{
+        const petDiaryId=req.params;
+        const diaryData=req.body;
+        try {
+            const result = await diaryService.putPetDiary(petDiaryId,diaryData)
+            res.status(statusCode.OK).send(util.success(statusCode.OK, result, ""))
+        } catch (err) {
+            console.error(err)
+            if (err.statusCode == null) {
+                err.statusCode = statusCode.INTERNAL_SERVER_ERROR;
+                err.responseMessage = responseMessage.INTERNAL_SERVER_ERROR;
+            }
+            return res.status(err.statusCode).send(util.fail(err.statusCode, err.responseMessage))
+        }
+    },
+    deletePetDiary:async(req,res)=>{
+        const petDiaryId=req.params;
+        try{
+            const result = await diaryService.deletePetDiary(petDiaryId,)
             res.status(statusCode.OK).send(util.success(statusCode.OK, result, ""))
         } catch (err) {
             console.error(err)
--- src/api/service/diaryService.ts
+++ src/api/service/diaryService.ts
@@ -6,7 +6,7 @@ import Pet from "../../models/pet/Pet"
 import { response } from "express"
 import PetDiary from "../../models/diary/PetDiary"
 import PetEmotions from "../../models/diary/PetEmotions"
-import { PetDiaryPageResDto} from "../../dto/petDiary/PetDiaryPageResDto"
+import { PetDiaryPageResDto } from "../../dto/petDiary/PetDiaryPageResDto"
 require("../../models/user/User")
 require("../../models/book/Book")
 require("../../models/pet/Pet")
@@ -89,11 +89,56 @@ module.exports = {
     getPetDiary: async (petDiaryId) => {
         try {
             const findPetDiary = PetDiary.findById(petDiaryId);
-            let petDiaryPageResDto = await new PetDiaryPageResDto(findPetDiary)
-            return petDiaryPageResDto
+            //let petDiaryPageResDto = await new PetDiaryPageResDto(findPetDiary) 이부분
+            // return petDiaryPageResDto
         } catch (err) {
             console.log(err)
             //throw err, noPetDiaryId
         }
+    },
+    putPetDiary: async (petDiaryId, diaryData) => {
+        try {
+            const findPetDiary = PetDiary.findById(petDiaryId);
+            const writeDate = new Date(diaryData.date)
+            writeDate.setDate(writeDate.getDate() + 1);
+            // console.log(FirstPartTableContents.findById(diaryData._id))
+
+            let newPetDiary = new PetDiary({
+                tableContents: diaryData._id,
+                //episode:FirstPartTableContents.findById(diaryData._id).length, 이부분 수정하기
+                date: writeDate,
+                imgs: diaryData.diaryImages,
+                title: diaryData.title,
+                contents: diaryData.contents
+
+            })
+            //save petinfo
+            let petN = diaryData.character.length
+            for (let i = 0; i < petN; i++) {
+                const petData = await Pet.findById(diaryData.character[0]._id).populate('_id')
+                newPetDiary.setPet(petData)
+                //save emotions
+                const petEmotion = new PetEmotions({
+                    pet: diaryData.character[0]._id,
+                    feeling: diaryData.character[0].feeling
+                })
+                newPetDiary.setPetEmotions(petEmotion)
+            }
+
+            console.log(newPetDiary)
+
+
+        } catch (err) {
+            console.log(err)
+        }
+
+    },
+    deletePetDiary:async(petDiaryId)=>{
+        try{
+            const findPetDiary=PetDiary.findById(petDiaryId);
+            //화 정렬 순서 맞추기
+            //해당 목차인것들 모두 가져오기. findPetDiary의 idx 뒤로 다 -1
+        }
     }
+    
 }
