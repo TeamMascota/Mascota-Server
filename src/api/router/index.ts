2d6b39fc17dbe6516ed10d1cf02d8f7f939c3d11
sol <dlthf555@gmail.com>
2021-07-12 00:04:00 +0900

    refactor : 로그인 api 수정

--- src/api/router/index.ts
+++ src/api/router/index.ts
@@ -1,9 +1,8 @@
 import express from "express"
 const router = express.Router();
 
-router.use('/rainbow',require('./rainbow')) //무지개 
-router.use('/user',require('./user'))//user
-router.use('/pet',require('./pet'))//pet
-//router.use('/book',require('./book'));//book
+// router.use('/rainbow',require('./rainbow')) //무지개 
+router.use('/user', require('./user'))//user
+router.use('/pet', require('./pet'))//pet
 
 module.exports = router;
