0dfda4358f9049dd5244f4070a31cbf33bb71fcc
hyunjong_96 <ahajongs@gmail.com>
2021-07-12 17:13:56 +0900

    refactor : s3 수정

--- src/api/router/pet/index.ts
+++ src/api/router/pet/index.ts
@@ -1,9 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var petController = require('../../controller/petController')
-var upload = require('../../../modules/multer')
 
 router.post('/register',petController.registerPet) //register pet
-router.post('/multer',upload.array('images',3),petController.setImages)
 
 module.exports = router;
