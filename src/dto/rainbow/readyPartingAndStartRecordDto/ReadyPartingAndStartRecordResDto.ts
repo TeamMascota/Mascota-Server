bc70d89e38b56d2ac935f3cdd52e728858a1117d
hyunjong_96 <ahajongs@gmail.com>
2021-07-13 20:31:20 +0900

    refactor : 1.5부 작가의 이별준비 시작 dto 필드값 변경

--- src/dto/rainbow/readyPartingAndStartRecordDto/ReadyPartingAndStartRecordResDto.ts
+++ src/dto/rainbow/readyPartingAndStartRecordDto/ReadyPartingAndStartRecordResDto.ts
@@ -15,9 +15,11 @@ export class ReadyPartingAndStartRecordResDto{
 export class BookInfoResDto{
     title = null;
     bookImg = null;
-
+    author = null;
     constructor(book : IBook){
+        console.log('!!!!!!! : '+book)
         this.title = book.title
         this.bookImg = book.imgs
+        this.author = book.author
     }
 }
