96d49c06f6a16ed2e0cfaa1face061c6a763b3c1
hyunjong_96 <ahajongs@gmail.com>
2021-07-15 01:48:19 +0900

    refactor : add memorise diaryId field

--- src/dto/rainbow/mainPageDto/RainbowMainPageResDto.ts
+++ src/dto/rainbow/mainPageDto/RainbowMainPageResDto.ts
@@ -27,6 +27,7 @@ export class RainbowMainPageResDto{
 }
 
 export class MemoriesResDto{
+    private diaryId;
     private title;
     private contents;
     private date;
@@ -35,11 +36,12 @@ export class MemoriesResDto{
     constructor(petDiaries : IPetDiary[], petId : IPet){
         const diaryIndex = this.getRandomMemoryIndex(petDiaries.length)
         const randomDiary = petDiaries[diaryIndex]
-        console.log('aaaa :'+petDiaries)
+        console.log('aaaa :'+randomDiary)
         console.log('bbbb :'+diaryIndex)
         console.log("랜덤된 petEmotion "+randomDiary.petEmotions.filter(petEmotion =>
             petEmotion.pet == petId)[0])
 
+        this.diaryId = randomDiary._id
         this.title = randomDiary.title
         this.contents = randomDiary.contents
         this.date = randomDiary.date
