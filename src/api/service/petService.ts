334c2abad3fc6b16a3a48bc40a8942219f6ea1a2
hyunjong_96 <ahajongs@gmail.com>
2021-07-16 21:16:52 +0900

    refactor : 반려동물 등록 이슈 해결

--- src/api/service/petService.ts
+++ src/api/service/petService.ts
@@ -30,27 +30,27 @@ module.exports = {
         
         console.log('petstype : '+typeof(pets))
         console.log('pets[0]type : '+typeof(pets[0]))
-        console.log('pets[0] : '+pets[0])
+        console.log('pets[0] : '+JSON.stringify(pets[0]))
         //console.log('############# : '+reqData[1].name)
         console.log('petsLength : '+pets.length)
         console.log('petsName : '+pets[0].name)
-        console.log('petsKind : '+pets[0].kind)
+        console.log('petsKind : '+pets[0].startDate)
         console.log('userId : '+userId)
         try{
         //error handling
-            const findUser = await User.findById(reqData.userId)
+            const findUser = await User.findById(userId)
             console.log('findUser : '+findUser)
             //console.log('image : '+images[0])
-            let pets=[];
-            const startDate= new Date(reqData[0].startDate)
+            let petsArr=[];
+            const startDate= new Date(pets[0].startDate)
             startDate.setDate(startDate.getDate() + 1);
-            for (let i = 0; i < reqData.pets.length; i++) {
+            for (let i = 0; i < pets.length; i++) {
                 let pet = new Pet({
-                    name: reqData.pets[i].name,
-                    kind: reqData.pets[i].kind,
-                    gender: reqData.pets[i].gender,
-                    imgs : "asdasd",
-                    user: mongoose.Types.ObjectId(reqData.userId),
+                    name: pets[i].name,
+                    kind: pets[i].kind,
+                    gender: pets[i].gender,
+                    imgs : "https://watcha.s3.ap-northeast-2.amazonaws.com/images/origin/%EC%B1%85+%EC%9D%B4%EB%AF%B8%EC%A7%80.jpg",
+                    user: mongoose.Types.ObjectId(userId),
                     rainbow: false,
                     startDate: new Date(startDate)
     
@@ -60,12 +60,12 @@ module.exports = {
                 });
                 await pet.save()
                 findUser.pets.push(pet)
-                pets[i]=pet;
+                petsArr[i]=pet;
             }
             await findUser.save()
             console.log(reqData);
             //db save
-            const saveInfo = pets.map(pet=>pet._id)
+            const saveInfo = petsArr.map(pet=>pet._id)
             return saveInfo
         }catch(err){
             console.log(err)
